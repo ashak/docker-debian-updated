a43847a217f12a00e62d0ca831eb5713057fb58a
Open Sourcery <open.source@opensourcery.uk>
2019-04-15 21:53:34 +0100

    Without --no-cache subsequent builds often do no updates.

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ DEBIAN_RELEASE=$1
 REGISTRY_IMAGE=$2
 IMAGE_TAG=${3:-$DEBIAN_RELEASE}
 
-docker build --rm -t opensourcery/debian:${DEBIAN_VERSION} --build-arg DEBIAN_RELEASE=${DEBIAN_RELEASE} .
+docker build --no-cache --rm -t opensourcery/debian:${DEBIAN_VERSION} --build-arg DEBIAN_RELEASE=${DEBIAN_RELEASE} .
 docker push ${REGISTRY_IMAGE}:${IMAGE_TAG}
